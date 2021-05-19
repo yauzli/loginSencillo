735332a76106b01a4786ca5036d2738330d31f5a
FlutLab.io <>
2021-05-19 17:49:38 +0000

    Switching to branch login2

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyloginApp extends StatelessWidget {
       title: 'web master',
       theme: ThemeData(
         brightness: Brightness.light,
-        primarySwatch: Colors.deepOrange[50],
+        primarySwatch: Colors.deepOrange[100],
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       darkTheme: ThemeData(
